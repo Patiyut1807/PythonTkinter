05a84aa9bbd9b2ba1b8e2253013ddaf707203e35
10Dev <47715101+Patiyut1807@users.noreply.github.com>
2021-11-14 16:46:23 +0700

    Delete Omakasa directory

--- Omakasa/file.py
+++
@@ -1,3 +0,0 @@
-f = open('Omakasa/Data/customer.txt', 'a')
-s = f.write('5'+'\n')
-f.close()
--- Omakasa/test.py
+++
@@ -1,6 +0,0 @@
-from tkinter import *
-
-window = Tk()
-fr= Frame().grid(column=2, row=2)
-text = Label(master=fr, text="55555")
-window.mainloop()
