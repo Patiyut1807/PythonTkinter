3625279f0839ba2bf4d96b528b85f8f8da7b1ac4
BarwSirati <sirati10546@gmail.com>
2021-11-17 22:45:27 +0700

    add

--- Omakasa/Omakasa.py
+++ Omakasa/Omakasa.py
@@ -1,4 +1,6 @@
 from tkinter import *
+from tkinter import ttk
+import time
 # ----------------------------------------------------------------
 
 
@@ -8,14 +10,15 @@ def Login_screen():
     Login['background'] = '#FFF'
     LogIn_txt = Label(Login, text='Login', font=(
         "Arial Bold", 25), background="#FD650D", fg='white').pack(pady=10, fill=X)
-    frame1 = Frame().pack(pady=5)
+
+    frame1 = Frame(Login).pack(pady=5)
     Id_in = StringVar()
     LogIn_user_txt = Label(frame1, text='Username',
                            font=("Arial", 14), bg='white').pack()
     LogIn_user_box = Entry(frame1, textvariable=Id_in, width=28, font=(
         "Arial", 12), bd='2').pack(ipady=4, ipadx=8)
 
-    frame2 = Frame().pack(pady=5)
+    frame2 = Frame(Login).pack(pady=5)
     Pass_in = StringVar()
     LogIn_pass_txt = Label(frame2, text='Password',
                            font=("Arial", 14), bg='white').pack()
@@ -29,7 +32,7 @@ def Login_screen():
         Login_file = open('Omakasa/Data/admin.txt', 'r')
         Id = Login_file.read().split('\n')
         for ID in Id:
-            if ID == ID_INPUT and 'omakasa6401' == PASS_INPUT :
+            if ID == ID_INPUT and 'omakasa6401' == PASS_INPUT:
                 ID_check = TRUE
         if ID_check:
             Login.destroy()
@@ -47,52 +50,189 @@ def Login_screen():
     Login.mainloop()
 
     # ----------------------------------------------------------------
-n = 0
 
-
+count = 1
 def main_screen():
-    root = Tk()
-    root.title('Omakasa')
+    main = Tk()
+    main.title('Omakasa')
+    main.geometry("720x600+350+100")
 
-    def add_row():
-        global n
-        frm_row = Frame(frm_table, width=650, height=15, pady=1, bg="#a8a5a5")
-        frm_row.pack()
-        table_n = Label(frm_row, text=n, font=("Arial", 13), width=2)
-        customer = Label(frm_row, text='Patipan', font=(
-            "Arial", 13), width=15, bg='white')
-        customer_n = Label(frm_row, text='5', font=("Arial", 13), width=3)
-        checkIn = Label(frm_row, text='11/11/64 -11:11',
-                        font=("Arial", 13), width=15, bg='white')
-        checkOut = Label(frm_row, text='11/11/64 -12:11',
-                         font=("Arial", 13), width=15)
-        lb_price = Label(frm_row, text='500', font=(
-            "Arial", 13), padx='25', width=4, bg='white')
-        btn_bill = Button(frm_row, text='เช็คบิล', padx=15,
-                          height=1, background="#49FF00")
 
-        table_n.grid(column=0, row=0)
-        customer.grid(column=1, row=0)
-        customer_n.grid(column=2, row=0)
-        checkIn.grid(column=3, row=0)
-        checkOut.grid(column=4, row=0)
-        lb_price.grid(column=5, row=0)
-        btn_bill.grid(column=6, row=0)
-        n += 1
-    LogIn_txt = Label(root, text='OMAKASA', font=(
-        "Arial Bold", 45), background="#FD650D", fg='white').place(x=10, y=10)
-    Search_box = Entry(root, font=("Arial", 16), width=20).place(x=370, y=90)
+    # ---------------------------------------------------------------
 
-    Search_button = Button(root, text='ค้นหา', font=(
-        "Arial Bold", 13), background="#FD650D", fg='white', command=add_row).place(x=620, y=87)
+    def add_screen():
+        add = Tk()
+        add.title('Omakasa-insert')
+        add.geometry("200x160+600+250")
+        add['bg'] = '#FD650D'
 
-    frm_table = Frame(root, bg="white")
-    frm_table.place(width=650, height=500, x=35, y=175)
+        frm_contain = Frame(add, bg='#FD650D', width=180, height=150)
+        frm_contain.place(x=5, y=25)
 
-    scr_bar = Scrollbar(frm_table)
-    scr_bar.pack(side=RIGHT, fill=Y)
-    root.geometry("720x720+300+0")
-    root.mainloop()
+        frm_name = Frame(frm_contain, bg='#FD650D', pady=10)
+        lb_name = Label(frm_name, text='Name', bg='white')
+        en_name = Entry(frm_name, text='', width=13, font=("Arial", 14))
+        frm_name.pack()
+        lb_name.grid(column=0, row=0)
+        en_name.grid(column=1, row=0)
+
+        frm_amount = Frame(frm_contain, bg='#FD650D', pady=10)
+        lb_amount = Label(frm_amount, text='Amount', bg='white')
+        en_amount = Entry(frm_amount, text='', width=12, font=("Arial", 14))
+        frm_amount.pack()
+        lb_amount.grid(column=0, row=0)
+        en_amount.grid(column=1, row=0)
+
+        def add_data():
+            global count
+            t = time.localtime(time.time())
+            tv_table.insert(parent='', index='end', iid=count, text='', values=(count, en_name.get(
+            ), en_amount.get(), f'{t.tm_mday}/{t.tm_mon}/{t.tm_year}-{t.tm_hour}:{t.tm_min}', '-', '-', '-'))
+            count += 1
+            add.destroy()
+        btn_add_data = Button(add, text='เพิ่ม', bg='white',
+                              relief=FLAT, width=10, height=1, command=add_data)
+        btn_add_data.place(x=65, y=120)
+
+        add.mainloop()
+
+    def remove_screen():
+        if not len(tv_table.selection()):
+            return
+        x = tv_table.selection()[0]
+
+        def del_data():
+            tv_table.delete(x)
+            delete_promt.destroy()
+
+        delete_promt = Tk()
+        delete_promt.title('Omakasa-Delete')
+        delete_promt.geometry("200x160+600+150")
+        delete_promt['bg'] = 'white'
+
+        lb_ask = Label(delete_promt, text='คุณต้องการ\nลบข้อมูลใช่หรือไม่?',
+                       bg='white', font=("Arial", 13), pady=15)
+        btn_confirm = Button(delete_promt, text='ตกลง',
+                             command=del_data, width=7, height=2)
+        btn_cancel = Button(delete_promt, text='ยกเลิก',
+                            width=7, height=2, command=delete_promt.destroy)
+
+        lb_ask.pack(pady=10)
+        btn_confirm.pack(side=LEFT, padx=20)
+        btn_cancel.pack(side=RIGHT, padx=20)
+
+    def edit_screen():
+        if not len(tv_table.selection()):
+            return
+        edit = Tk()
+        edit.title('Omakasa-edit')
+        edit.geometry("200x160+600+150")
+        edit['bg'] = '#FD650D'
+        frm_contain = Frame(edit, bg='#FD650D', width=180, height=150)
+        frm_contain.place(x=5, y=25)
+
+        frm_name = Frame(frm_contain, bg='#FD650D', pady=10)
+        lb_name = Label(frm_name, text='Name', bg='white')
+        en_name = Entry(frm_name, text='', width=13, font=("Arial", 14))
+        frm_name.pack()
+        lb_name.grid(column=0, row=0)
+        en_name.grid(column=1, row=0)
+
+        frm_amount = Frame(frm_contain, bg='#FD650D', pady=10)
+        lb_amount = Label(frm_amount, text='Amount', bg='white')
+        en_amount = Entry(frm_amount, text='', width=12, font=("Arial", 14))
+        frm_amount.pack()
+        lb_amount.grid(column=0, row=0)
+        en_amount.grid(column=1, row=0)
+
+        def edit_data():
+            t=time.localtime(time.time())
+            select_item = tv_table.selection()[0]
+            tv_table.item(select_item,text='',values=(2, 'csddssd', 'dsadas',' ',f'{t.tm_mday}/{t.tm_mon}/{t.tm_year}-{t.tm_hour}:{t.tm_min}', '-', '-'))
+        btn_edit_data = Button(edit,text='แก้ไข',bg='white',relief=FLAT,width=10,height=1,command=edit_data)
+        btn_edit_data.place(x=65,y=120)
+        edit.mainloop()
+
+    def check_bill():
+        if not len(tv_table.selection()):
+            return
+
+    def new_login():
+        main.destroy()
+        Login_screen()
+    # -----------------------------------------------------------------
+
+    menu_bar = Menu(main)
+    user_menu = Menu(menu_bar,tearoff=0)
+    user_menu.add_command(label='Edit Username')
+    user_menu.add_command(label='Sign Out',command= new_login)
+    user_menu.add_command(label='Exit',command=main.destroy)
+
+    menu_bar.add_cascade(label="User", menu=user_menu)
+
+    Logo_txt = Label(main, text='OMAKASA', font=("Arial Bold", 45),
+                     background="#FD650D", fg='white').place(x=10, y=10)
+    fr = Frame(main, bg='white', width=300, height=3)
+    fr.place(x=15, y=75)
+
+    frm_contain_btn = Frame(main, bg='white', width=60, height=80,)
+    global btn_add
+    btn_add = Button(frm_contain_btn, text='เพิ่ม', font=("Arial bold", 12), relief=RIDGE, width=5, bg='#2ff764',
+                     fg='white', bd=2, activebackground='#2ff764', activeforeground='white', command=add_screen)
+    global btn_edit
+    btn_edit = Button(frm_contain_btn, text='แก้ไข', font=("Arial bold", 12), relief=RIDGE, width=5, bg='#fceb4c', fg='white', command=edit_screen
+                      )
+    global btn_del
+    btn_del = Button(frm_contain_btn, text='ลบ', font=("Arial bold", 12), relief=RIDGE, width=5, bg='#ed3737', fg='white', command=remove_screen
+                     )
+
+    frm_contain_btn.place(x=485, y=135)
+    btn_add.grid(column=0, row=0)
+    btn_edit.grid(column=1, row=0)
+    btn_del.grid(column=2, row=0)
+
+    text_search = StringVar()
+    Search_box = Entry(main, textvariable=text_search, font=(
+        "Arial", 16), width=20).place(x=370, y=95)
+    Search_button = Button(main, text='ค้นหา', font=(
+        "Arial Bold", 13), background="#FD650D", fg='white').place(x=620, y=92)
+
+    frm_table = Frame(main, bg="white")
+    frm_table.place(width=660, height=350, x=30, y=175)
+
+    tv_table = ttk.Treeview(frm_table)
+    tv_table['columns'] = ('No.', 'Name', 'Amount',
+                           'CheckIn', 'CheckOut', 'Price', 'Total')
+
+    tv_table.heading('#0', text='', anchor=CENTER)
+    tv_table.heading('No.', text='No', anchor=CENTER)
+    tv_table.heading('Name', text='Name', anchor=CENTER)
+    tv_table.heading('Amount', text='Amount', anchor=CENTER)
+    tv_table.heading('CheckIn', text='CheckIn', anchor=CENTER)
+    tv_table.heading('CheckOut', text='CheckOut', anchor=CENTER)
+    tv_table.heading('Price', text='Price', anchor=CENTER)
+    tv_table.heading('Total', text='Total', anchor=CENTER)
+
+    tv_table.column('#0', width=0, stretch=NO)
+    tv_table.column('No.', anchor=CENTER, width=27)
+    tv_table.column('Name', anchor=CENTER, width=160)
+    tv_table.column('Amount', anchor=CENTER, width=60)
+    tv_table.column('CheckIn', anchor=CENTER, width=125)
+    tv_table.column('CheckOut', anchor=CENTER, width=125)
+    tv_table.column('Price', anchor=CENTER, width=80)
+    tv_table.column('Total', anchor=CENTER, width=80)
+    tv_table.place(x=0, y=0, height=350)
+
+    btn_check_bill = Button(main, width=10, height=1, fg='white', bg='#FD650D',
+                            text="คิดเงิน", font=('Arail bold', 15), command=check_bill)
+    btn_check_bill.place(x=570, y=540)
+
+# --------------------------------------------
+
+
+# --------------------------------------------
+    main.config(menu=menu_bar)
+    main.mainloop()
 
 
 Login_screen()
--- Omakasa/main.py
+++ Omakasa/main.py
@@ -2,159 +2,156 @@ from tkinter import *
 from tkinter import ttk
 import time
 
-count = 0
+
+count = 1
 def main_screen():
     main = Tk()
     main.title('Omakasa')
-    main.geometry("720x600+300+0")
-    #---------------------------------------------------------------
-    global add_screen_state
-    add_screen_state=0
-    
+    main.geometry("720x600+350+100")
+
+
+    # ---------------------------------------------------------------
+
     def add_screen():
         add = Tk()
         add.title('Omakasa-insert')
-        add.geometry("200x160+600+150")
-        add['bg']='#FD650D' 
-        def add_screen_check():
-            global add_screen_state
-            if add_screen_state == 1 :
-                add.destroy()
-        # add_screen_check()
-        
-        global add_screen_state
-        add_screen_state = 1
-        frm_contain = Frame(add,bg='#FD650D',width=180,height=150)
-        frm_contain.place(x=5,y=25)
-       
-        frm_name = Frame(frm_contain,bg='#FD650D',pady=10)
-        lb_name = Label(frm_name,text='Name',bg='white')
-        en_name = Entry(frm_name,text='',width=13,font=("Arial",14))
+        add.geometry("200x160+600+250")
+        add['bg'] = '#FD650D'
+
+        frm_contain = Frame(add, bg='#FD650D', width=180, height=150)
+        frm_contain.place(x=5, y=25)
+
+        frm_name = Frame(frm_contain, bg='#FD650D', pady=10)
+        lb_name = Label(frm_name, text='Name', bg='white')
+        en_name = Entry(frm_name, text='', width=13, font=("Arial", 14))
         frm_name.pack()
-        lb_name.grid(column=0,row=0)
-        en_name.grid(column=1,row=0)
-    
-        frm_amount = Frame(frm_contain,bg='#FD650D',pady=10)
-        lb_amount = Label(frm_amount,text='Amount',bg='white')
-        en_amount = Entry(frm_amount,text='',width=12,font=("Arial",14))
+        lb_name.grid(column=0, row=0)
+        en_name.grid(column=1, row=0)
+
+        frm_amount = Frame(frm_contain, bg='#FD650D', pady=10)
+        lb_amount = Label(frm_amount, text='Amount', bg='white')
+        en_amount = Entry(frm_amount, text='', width=12, font=("Arial", 14))
         frm_amount.pack()
-        lb_amount.grid(column=0,row=0)
-        en_amount.grid(column=1,row=0)
+        lb_amount.grid(column=0, row=0)
+        en_amount.grid(column=1, row=0)
+
         def add_data():
             global count
             t = time.localtime(time.time())
-            tv_table.insert(parent='', index='end', iid=count, text='', values=(count,en_name.get(), en_amount.get(),f'{t.tm_mday}/{t.tm_mon}/{t.tm_year}-{t.tm_hour}:{t.tm_min}','-','-','-'))
-            count+=1
-            global add_screen_state
-            add_screen_state = 0
+            tv_table.insert(parent='', index='end', iid=count, text='', values=(count, en_name.get(
+            ), en_amount.get(), f'{t.tm_mday}/{t.tm_mon}/{t.tm_year}-{t.tm_hour}:{t.tm_min}', '-', '-', '-'))
+            count += 1
             add.destroy()
-        btn_add_data = Button(add,text='เพิ่ม',bg='white',relief=FLAT,width=10,height=1,command=add_data)
-        btn_add_data.place(x=65,y=120)
-    
+        btn_add_data = Button(add, text='เพิ่ม', bg='white',
+                              relief=FLAT, width=10, height=1, command=add_data)
+        btn_add_data.place(x=65, y=120)
+
         add.mainloop()
-        
+
     def remove_screen():
-        x=tv_table.selection()[0]
-        def del_data(): 
+        if not len(tv_table.selection()):
+            return
+        x = tv_table.selection()[0]
+
+        def del_data():
             tv_table.delete(x)
             delete_promt.destroy()
-        if int(x) >= 0 :
-            delete_promt = Tk()
-            delete_promt.title('Omakasa-Delete')
-            delete_promt.geometry("200x160+600+150")
-            delete_promt['bg'] = 'white'
-        
-            lb_ask = Label(delete_promt,text='คุณต้องการ\nลบข้อมูลใช่หรือไม่?',bg='white',font=("Arial", 13))
-            btn_confirm = Button(delete_promt,text='ตกลง',command=del_data,width=5,height=2)
-            btn_cancel = Button(delete_promt,text='ยกเลิก',width=5,height=2,command=delete_promt.destroy)
-        
-            lb_ask.pack(pady=10)
-            btn_confirm.pack(side=LEFT,padx=20)
-            btn_cancel.pack(side=RIGHT,padx=20)
+
+        delete_promt = Tk()
+        delete_promt.title('Omakasa-Delete')
+        delete_promt.geometry("200x160+600+150")
+        delete_promt['bg'] = 'white'
+
+        lb_ask = Label(delete_promt, text='คุณต้องการ\nลบข้อมูลใช่หรือไม่?',
+                       bg='white', font=("Arial", 13), pady=15)
+        btn_confirm = Button(delete_promt, text='ตกลง',
+                             command=del_data, width=7, height=2)
+        btn_cancel = Button(delete_promt, text='ยกเลิก',
+                            width=7, height=2, command=delete_promt.destroy)
+
+        lb_ask.pack(pady=10)
+        btn_confirm.pack(side=LEFT, padx=20)
+        btn_cancel.pack(side=RIGHT, padx=20)
+
     def edit_screen():
+        if not len(tv_table.selection()):
+            return
         edit = Tk()
         edit.title('Omakasa-edit')
         edit.geometry("200x160+600+150")
-        edit['bg']='#FD650D' 
-        frm_contain = Frame(edit,bg='#FD650D',width=180,height=150)
-        frm_contain.place(x=5,y=25)
-       
-        frm_name = Frame(frm_contain,bg='#FD650D',pady=10)
-        lb_name = Label(frm_name,text='Name',bg='white')
-        en_name = Entry(frm_name,text='',width=13,font=("Arial",14))
+        edit['bg'] = '#FD650D'
+        frm_contain = Frame(edit, bg='#FD650D', width=180, height=150)
+        frm_contain.place(x=5, y=25)
+
+        frm_name = Frame(frm_contain, bg='#FD650D', pady=10)
+        lb_name = Label(frm_name, text='Name', bg='white')
+        en_name = Entry(frm_name, text='', width=13, font=("Arial", 14))
         frm_name.pack()
-        lb_name.grid(column=0,row=0)
-        en_name.grid(column=1,row=0)
-    
-        frm_amount = Frame(frm_contain,bg='#FD650D',pady=10)
-        lb_amount = Label(frm_amount,text='Amount',bg='white')
-        en_amount = Entry(frm_amount,text='',width=12,font=("Arial",14))
+        lb_name.grid(column=0, row=0)
+        en_name.grid(column=1, row=0)
+
+        frm_amount = Frame(frm_contain, bg='#FD650D', pady=10)
+        lb_amount = Label(frm_amount, text='Amount', bg='white')
+        en_amount = Entry(frm_amount, text='', width=12, font=("Arial", 14))
         frm_amount.pack()
-        lb_amount.grid(column=0,row=0)
-        en_amount.grid(column=1,row=0)
+        lb_amount.grid(column=0, row=0)
+        en_amount.grid(column=1, row=0)
+
         def edit_data():
-            global count
-            count+=1
-            global add_screen_state
-            add_screen_state = 0
-            add.destroy()
-        btn_add_data = Button(add,text='เพิ่ม',bg='white',relief=FLAT,width=10,height=1,command=add_data)
-        btn_add_data.place(x=65,y=120)
-    
-        
+            t=time.localtime(time.time())
+            select_item = tv_table.selection()[0]
+            tv_table.item(select_item,text='',values=(2, 'csddssd', 'dsadas',' ',f'{t.tm_mday}/{t.tm_mon}/{t.tm_year}-{t.tm_hour}:{t.tm_min}', '-', '-'))
+        btn_edit_data = Button(edit,text='แก้ไข',bg='white',relief=FLAT,width=10,height=1,command=edit_data)
+        btn_edit_data.place(x=65,y=120)
         edit.mainloop()
-        
-        
-        
-    #-----------------------------------------------------------------
-    Logo_txt = Label(main,text='OMAKASA',font=("Arial Bold", 45),background="#FD650D",fg='white').place(x=10,y=10)
-    fr = Frame(main,bg='white',width=300,height=3)
-    fr.place(x=15,y=75)
-    
-    frm_contain_btn = Frame(main,bg='white',width=60,height=80,)
+
+    def check_bill():
+        if not len(tv_table.selection()):
+            return
+
+    # -----------------------------------------------------------------
+
+    menu_bar = Menu(main)
+    user_menu = Menu(menu_bar,tearoff=0)
+    user_menu.add_command(label='Edit Username')
+    user_menu.add_command(label='Sign Out',Login_screen)
+    user_menu.add_command(label='Exit',command=main.destroy)
+
+    menu_bar.add_cascade(label="User", menu=user_menu)
+
+    Logo_txt = Label(main, text='OMAKASA', font=("Arial Bold", 45),
+                     background="#FD650D", fg='white').place(x=10, y=10)
+    fr = Frame(main, bg='white', width=300, height=3)
+    fr.place(x=15, y=75)
+
+    frm_contain_btn = Frame(main, bg='white', width=60, height=80,)
     global btn_add
-    btn_add = Button(frm_contain_btn,text='เพิ่ม'
-                     ,font=("Arial bold", 12)
-                     ,relief=RIDGE,width=5
-                     ,bg='#2ff764'
-                     ,fg='white'
-                     ,bd=2
-                     ,activebackground='#2ff764'
-                     ,activeforeground='white'
-                     ,command=add_screen)
+    btn_add = Button(frm_contain_btn, text='เพิ่ม', font=("Arial bold", 12), relief=RIDGE, width=5, bg='#2ff764',
+                     fg='white', bd=2, activebackground='#2ff764', activeforeground='white', command=add_screen)
     global btn_edit
-    btn_edit = Button(frm_contain_btn
-                      ,text='แก้ไข'
-                      ,font=("Arial bold", 12)
-                      ,relief=RIDGE
-                      ,width=5,bg='#fceb4c'
-                      ,fg='white'
-                      ,command=edit_screen
+    btn_edit = Button(frm_contain_btn, text='แก้ไข', font=("Arial bold", 12), relief=RIDGE, width=5, bg='#fceb4c', fg='white', command=edit_screen
                       )
     global btn_del
-    btn_del = Button(frm_contain_btn,text='ลบ' 
-                     ,font=("Arial bold", 12)
-                     ,relief=RIDGE
-                     ,width=5
-                     ,bg='#ed3737'
-                     ,fg='white'
-                     ,command=remove_screen
+    btn_del = Button(frm_contain_btn, text='ลบ', font=("Arial bold", 12), relief=RIDGE, width=5, bg='#ed3737', fg='white', command=remove_screen
                      )
-    
-    frm_contain_btn.place(x=485,y=135)
-    btn_add.grid(column=0,row=0)
-    btn_edit.grid(column=1,row=0)
-    btn_del.grid(column=2,row=0)
-    
+
+    frm_contain_btn.place(x=485, y=135)
+    btn_add.grid(column=0, row=0)
+    btn_edit.grid(column=1, row=0)
+    btn_del.grid(column=2, row=0)
+
     text_search = StringVar()
-    Search_box  = Entry(main,textvariable=text_search, font=("Arial", 16),width=20).place(x=370,y=95) 
-    Search_button = Button(main,text='ค้นหา',font=("Arial Bold", 13),background="#FD650D",fg='white').place(x=620,y=92)
-    
-    frm_table =Frame(main, bg="white")
+    Search_box = Entry(main, textvariable=text_search, font=(
+        "Arial", 16), width=20).place(x=370, y=95)
+    Search_button = Button(main, text='ค้นหา', font=(
+        "Arial Bold", 13), background="#FD650D", fg='white').place(x=620, y=92)
+
+    frm_table = Frame(main, bg="white")
     frm_table.place(width=660, height=350, x=30, y=175)
-    
+
     tv_table = ttk.Treeview(frm_table)
-    tv_table['columns'] = ('No.', 'Name', 'Amount', 'CheckIn','CheckOut', 'Price', 'Total')
+    tv_table['columns'] = ('No.', 'Name', 'Amount',
+                           'CheckIn', 'CheckOut', 'Price', 'Total')
 
     tv_table.heading('#0', text='', anchor=CENTER)
     tv_table.heading('No.', text='No', anchor=CENTER)
@@ -166,29 +163,23 @@ def main_screen():
     tv_table.heading('Total', text='Total', anchor=CENTER)
 
     tv_table.column('#0', width=0, stretch=NO)
-    tv_table.column('No.', anchor=CENTER,width=27)
+    tv_table.column('No.', anchor=CENTER, width=27)
     tv_table.column('Name', anchor=CENTER, width=160)
     tv_table.column('Amount', anchor=CENTER, width=60)
     tv_table.column('CheckIn', anchor=CENTER, width=125)
     tv_table.column('CheckOut', anchor=CENTER, width=125)
     tv_table.column('Price', anchor=CENTER, width=80)
     tv_table.column('Total', anchor=CENTER, width=80)
-    tv_table.place(x=0,y=0,height=350)
-    
-    def active():
-        btn_edit.config(state=ACTIVE,activebackground='#fceb4c',activeforeground='white')
-    
-    btn_check_bill = Button(main,width=10,height=1,fg='white',bg='#FD650D',text="คิดเงิน",font=('Arail bold',15),command=active)
-    btn_check_bill.place(x=570,y=540)
-    
-#--------------------------------------------
-    
-    
-    
-    
-    
-#--------------------------------------------
-    
-    main.mainloop() 
-    
+    tv_table.place(x=0, y=0, height=350)
+
+    btn_check_bill = Button(main, width=10, height=1, fg='white', bg='#FD650D',
+                            text="คิดเงิน", font=('Arail bold', 15), command=check_bill)
+    btn_check_bill.place(x=570, y=540)
+
+# --------------------------------------------
+
+
+# --------------------------------------------
+    main.config(menu=menu_bar)
+    main.mainloop()
 main_screen()
